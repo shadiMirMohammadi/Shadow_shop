73822180e2b1a106df4cab4908e0f030eed08e2e
shadiMirMohammadi <shadi.Mir.Mohammadi.80@gmail.com>
2024-06-12 18:28:21 +0330

    fix (scripts) : add file js
    delete the previous js files and add the
     new js files in the script folder
    
    breaking : a NullReferenceException was
    encountered when using the package
     so a round of js files were removed and added

---
+++ Shadow_shop/Scripts/site.js
@@ -0,0 +1,89 @@
+﻿
+function imagePreview(input) {
+    if (input.files && input.files[0]) {
+        var filerdr = new FileReader();
+        filerdr.onload = function (e) {
+            jQuery('#newImagePreview').attr('src', e.target.result);
+        }
+        filerdr.readAsDataURL(input.files[0]);
+    }
+}
+
+function DeleteCategories(id) {
+    $.ajax({
+
+        /*url: '@Url.Action("/Categories/Delete")',*/
+        url: "/Categories/DeleteCategories/",
+        type: 'POST',
+        data: { id: id },
+
+        success: function (res) {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        },
+        error: function () {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        }
+    });
+}
+
+function DeleteArticles(id) {
+    $.ajax({
+
+        /*url: '@Url.Action("/Categories/Delete")',*/
+        url: "/Articles/DeleteArticles/",
+        type: 'POST',
+        data: { id: id },
+
+        success: function (res) {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        },
+        error: function () {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        }
+    });
+}
+
+function DeleteComment(id) {
+    $.ajax({
+
+        /*url: '@Url.Action("/Categories/Delete")',*/
+        url: "/Comments/DeleteComment/",
+        type: 'POST',
+        data: { id: id },
+
+        success: function (res) {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        },
+        error: function () {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        }
+    });
+}
+
+function UserDelete(id) {
+    $.ajax({
+
+        /*url: '@Url.Action("/Categories/Delete")',*/
+        url: "/UserRoles/UserDelete/",
+        type: 'POST',
+        data: { id: id },
+
+        success: function (res) {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        },
+        error: function () {
+            alert("حذف انجام شد(مجددا تازه سازی کنید)");
+        }
+    });
+}
+
+//success: function (res) {
+//    $("#List").html(res);
+//},
+
+
+$(function () {
+    $('#Content').ckeditor();
+});
+
+
