1d806739308bf645a53a9acff1d006fb3d48c6b1
shadiMirMohammadi <shadi.Mir.Mohammadi.80@gmail.com>
2024-07-23 11:31:23 +0330

    -feat(ModelsLayer): no change

---
+++ Shadow_shop.ModelsLayer/Migrations/202407121733006_CreateDB2.cs
@@ -0,0 +1,18 @@
+﻿namespace Shadow_shop.ModelsLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDB2 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.T_ProductCategory", "ParentCategoryName", c => c.String(maxLength: 50));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.T_ProductCategory", "ParentCategoryName");
+        }
+    }
+}
---
+++ Shadow_shop.ModelsLayer/Migrations/202407131620003_CreateDB3.cs
@@ -0,0 +1,18 @@
+﻿namespace Shadow_shop.ModelsLayer.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class CreateDB3 : DbMigration
+    {
+        public override void Up()
+        {
+            DropColumn("dbo.T_ProductCategory", "ParentCategoryName");
+        }
+        
+        public override void Down()
+        {
+            AddColumn("dbo.T_ProductCategory", "ParentCategoryName", c => c.String(maxLength: 50));
+        }
+    }
+}
